c9183862efa3cb59c9e2923a5341ce292c9a29de
zengshengliu <zengshengliu@gmail.com>
2012-12-05 14:57:54 -0500

    Remove Hello World message from strings.xml

--- gen/com/intuitive/yummy/R.java
+++ gen/com/intuitive/yummy/R.java
@@ -50,37 +50,36 @@ public final class R {
         public static final int activity_search_results=0x7f060003;
     }
     public static final class string {
-        public static final int activity_login_button_register=0x7f040012;
-        public static final int activity_login_button_signIn=0x7f040010;
-        public static final int activity_login_hint_password=0x7f04000e;
-        public static final int activity_login_hint_username=0x7f04000d;
-        public static final int activity_login_textView_register=0x7f040011;
-        public static final int activity_login_textView_signIn=0x7f04000f;
-        public static final int activity_main_button_login=0x7f040004;
-        public static final int activity_main_button_register=0x7f040007;
-        public static final int activity_main_button_search=0x7f040006;
-        public static final int activity_main_button_searchNearby=0x7f040005;
+        public static final int activity_login_button_register=0x7f040011;
+        public static final int activity_login_button_signIn=0x7f04000f;
+        public static final int activity_login_hint_password=0x7f04000d;
+        public static final int activity_login_hint_username=0x7f04000c;
+        public static final int activity_login_textView_register=0x7f040010;
+        public static final int activity_login_textView_signIn=0x7f04000e;
+        public static final int activity_main_button_login=0x7f040003;
+        public static final int activity_main_button_register=0x7f040006;
+        public static final int activity_main_button_search=0x7f040005;
+        public static final int activity_main_button_searchNearby=0x7f040004;
         public static final int app_name=0x7f040000;
-        public static final int food_type=0x7f04000b;
-        public static final int hello_world=0x7f040001;
-        public static final int location=0x7f04000a;
-        public static final int menu_settings=0x7f040002;
-        public static final int search_by_location=0x7f040008;
-        public static final int search_by_type=0x7f040009;
+        public static final int food_type=0x7f04000a;
+        public static final int location=0x7f040009;
+        public static final int menu_settings=0x7f040001;
+        public static final int search_by_location=0x7f040007;
+        public static final int search_by_type=0x7f040008;
         /**  LoginActivity 
          */
-        public static final int title_activity_login=0x7f04000c;
+        public static final int title_activity_login=0x7f04000b;
         /**  MainActivity 
          */
-        public static final int title_activity_main=0x7f040003;
-        public static final int title_activity_search=0x7f040013;
-        public static final int title_activity_search_results=0x7f040014;
+        public static final int title_activity_main=0x7f040002;
+        public static final int title_activity_search=0x7f040012;
+        public static final int title_activity_search_results=0x7f040013;
         /**  VendorActivity 
          */
-        public static final int vendor1=0x7f040015;
-        public static final int vendor_description=0x7f040018;
-        public static final int view_menu=0x7f040016;
-        public static final int view_review=0x7f040017;
+        public static final int vendor1=0x7f040014;
+        public static final int vendor_description=0x7f040017;
+        public static final int view_menu=0x7f040015;
+        public static final int view_review=0x7f040016;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
