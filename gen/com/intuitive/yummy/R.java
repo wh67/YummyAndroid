85b44e348f1d5a7eb9113d433c68d3c45a625142
Fifi <wh67@drexel.edu>
2012-12-05 14:33:29 -0500

    added part of vendor screen

--- gen/com/intuitive/yummy/R.java
+++ gen/com/intuitive/yummy/R.java
@@ -17,18 +17,23 @@ public final class R {
     public static final class id {
         public static final int button_login=0x7f070005;
         public static final int button_register=0x7f070003;
+        public static final int button_review=0x7f07000d;
         public static final int button_signIn=0x7f070002;
         public static final int food_type=0x7f070009;
         public static final int header=0x7f070004;
         public static final int location=0x7f070007;
-        public static final int menu_settings=0x7f07000b;
+        public static final int menu_settings=0x7f07000e;
         public static final int password=0x7f070001;
         public static final int search_by_location=0x7f070008;
         public static final int search_by_type=0x7f07000a;
         public static final int textView1=0x7f070006;
         public static final int username=0x7f070000;
+        public static final int vendorDescription=0x7f07000c;
+        public static final int vendorName=0x7f07000b;
     }
     public static final class id0 {
+        public static final int button_menu=0x7f080001;
+        public static final int button_review=0x7f080002;
         public static final int button_search=0x7f080000;
     }
     public static final class layout {
@@ -36,6 +41,7 @@ public final class R {
         public static final int activity_main=0x7f030001;
         public static final int activity_search=0x7f030002;
         public static final int activity_search_results=0x7f030003;
+        public static final int activity_vendor=0x7f030004;
     }
     public static final class menu {
         public static final int activity_login=0x7f060000;
@@ -44,31 +50,37 @@ public final class R {
         public static final int activity_search_results=0x7f060003;
     }
     public static final class string {
-        public static final int activity_login_button_register=0x7f040010;
-        public static final int activity_login_button_signIn=0x7f04000e;
-        public static final int activity_login_hint_password=0x7f04000c;
-        public static final int activity_login_hint_username=0x7f04000b;
-        public static final int activity_login_textView_register=0x7f04000f;
-        public static final int activity_login_textView_signIn=0x7f04000d;
+        public static final int activity_login_button_register=0x7f040012;
+        public static final int activity_login_button_signIn=0x7f040010;
+        public static final int activity_login_hint_password=0x7f04000e;
+        public static final int activity_login_hint_username=0x7f04000d;
+        public static final int activity_login_textView_register=0x7f040011;
+        public static final int activity_login_textView_signIn=0x7f04000f;
         public static final int activity_main_button_login=0x7f040004;
         public static final int activity_main_button_register=0x7f040007;
         public static final int activity_main_button_search=0x7f040006;
         public static final int activity_main_button_searchNearby=0x7f040005;
         public static final int app_name=0x7f040000;
-        public static final int food_type=0x7f040014;
+        public static final int food_type=0x7f04000b;
         public static final int hello_world=0x7f040001;
-        public static final int location=0x7f040013;
+        public static final int location=0x7f04000a;
         public static final int menu_settings=0x7f040002;
         public static final int search_by_location=0x7f040008;
         public static final int search_by_type=0x7f040009;
         /**  LoginActivity 
          */
-        public static final int title_activity_login=0x7f04000a;
+        public static final int title_activity_login=0x7f04000c;
         /**  MainActivity 
          */
         public static final int title_activity_main=0x7f040003;
-        public static final int title_activity_search=0x7f040011;
-        public static final int title_activity_search_results=0x7f040012;
+        public static final int title_activity_search=0x7f040013;
+        public static final int title_activity_search_results=0x7f040014;
+        /**  VendorActivity 
+         */
+        public static final int vendor1=0x7f040015;
+        public static final int vendor_description=0x7f040018;
+        public static final int view_menu=0x7f040016;
+        public static final int view_review=0x7f040017;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
