0be0533ae3ef94f002bea1809853c5df7dfd5d18
zengshengliu <zengshengliu@gmail.com>
2012-12-05 23:14:45 -0500

    Update SearchResultActivity
    Get list of vendors name from Vendor class

--- src/com/intuitive/yummy/SearchResultsActivity.java
+++ src/com/intuitive/yummy/SearchResultsActivity.java
@@ -10,14 +10,31 @@ import android.widget.ListView;
 
 public class SearchResultsActivity extends ListActivity {
 	
+	private String [] values = new String[7];
+	/*
 	private String[] values = new String[] {
     		"Jack's Pizza", "Sally's Subs", "Bob's Burritos",
             "Larry's Lasagna", "Helga's Hell Kitchen", "Carlos' Cuisine", 
             "Isabel's Ice Cream" 
     };
+    */
+	
+	Vendor[] vendors = {new Vendor("Jack's Pizza", "", "", null, false),
+						new Vendor("Sally's Subs", "", "", null, false),
+						new Vendor("Bob's Burritos", "", "", null, false),
+						new Vendor("Larry's Lasagna", "", "", null, false),
+						new Vendor("Helga's Hell Kitchen", "", "", null, false),
+						new Vendor("Carlos' Cuisine", "", "", null, false),
+						new Vendor("Isabel's Ice Cream", "", "", null, false)};
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+    	
+    	for (int i = 0; i < vendors.length; i++)
+    	{
+    		values[i] = vendors[i].getName();
+    	}
+    	
         super.onCreate(savedInstanceState);
         
         Intent intent = getIntent();
