e0b1ad9fbd10ecf859db28fb9f9db71ddedeecc8
Fifi <wh67@drexel.edu>
2013-02-25 19:51:00 -0500

    small changes

--- src/com/intuitive/yummy/MenuActivity.java
+++ src/com/intuitive/yummy/MenuActivity.java
@@ -13,44 +13,32 @@ import android.view.View.OnClickListener;
 
 public class MenuActivity extends ListActivity {
 	private com.intuitive.yummy.Menu menu;
+	private ListView listView;
+
 		
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
         
         Intent intent = getIntent();
         menu = (com.intuitive.yummy.Menu)intent.getSerializableExtra("Menu");
-        String[] menuItemsName;
-        if (menu == null)
-        {
-        	menuItemsName = new String[1];
-        	menuItemsName[0] = "No menu available.";
-        }
-        else if (menu.getMenuItem().size() == 0)
-        {
-        	menuItemsName = new String[1];
-        	menuItemsName[0] = "No item available.";
-        }
-        else
-        {
-        	menuItemsName = new String[menu.getMenuItem().size()];
-	        for (int i = 0; i < menu.getMenuItem().size(); i++)
-	        {
-	        	menuItemsName[i] = menu.getMenuItem().get(i).getName();
-	        }
-        }
+        MenuItem[] itemsArray = new MenuItem[menu.getMenuItem().size()];
+        menu.getMenuItem().toArray(itemsArray);
         
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-        		android.R.layout.simple_list_item_1, menuItemsName);
-        setListAdapter(adapter);
-   
-    }
-
+        System.out.println("creating adapter");
+        MenuItemAdapter adapter = new MenuItemAdapter(this, R.layout.list_menuitem, itemsArray);
+        listView = (ListView)findViewById(R.id.listMenuItem);
+        listView.setAdapter(adapter);
+            }
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_search_results, menu);
         return true;
     }
+
     
     public void cart(View v){
     	Intent intent = new Intent(this, CartActivity.class);
