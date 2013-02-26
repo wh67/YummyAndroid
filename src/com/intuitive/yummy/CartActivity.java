93e50ee482d2fbaa1e646d5383da68d5a95a13a0
Fifi <wh67@drexel.edu>
2013-02-26 01:21:04 -0500

    E13 Complete

--- src/com/intuitive/yummy/CartActivity.java
+++ src/com/intuitive/yummy/CartActivity.java
@@ -2,36 +2,50 @@ package com.intuitive.yummy;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
-import android.view.Menu;
-import android.view.ViewGroup.LayoutParams;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.TableRow.LayoutParams;
+import android.view.Menu;
+import android.view.View;
 
 public class CartActivity extends Activity {
 
-	String[] addedItems = {"16 inch Cheese Pizza", "16 inch Pepperoni Pizza", "16 inch Sausage Pizza", "Cheese Pizza Slice"};
-	double[] price = {14.99, 16.99, 16.99, 2.99};
+	String[] addedItems = { "16 inch Cheese Pizza", "16 inch Pepperoni Pizza",
+			"16 inch Sausage Pizza", "Cheese Pizza Slice" };
+	double[] price = { 10, 11, 12, 1.5 };
+
 	TableLayout t1;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_cart);
-		t1 = (TableLayout)findViewById(R.id.table1);
-		
-		for (int i = 0; i < addedItems.length; i++){
+
+		//adding cell to each row with dummy data
+		t1 = (TableLayout) findViewById(R.id.table1);
+		for (int i = 0; i < addedItems.length; i++) {
 			TableRow tr = new TableRow(this);
 			TextView tv1 = new TextView(this);
 			TextView tv2 = new TextView(this);
 			TextView tv3 = new TextView(this);
-			
-			createView(tr,tv1,Integer.toString(1));
-			createView(tr,tv2,addedItems[i]);
-			createView(tr,tv3,Double.toString(price[i]));
+			createView(tr, tv1, Integer.toString(1));
+			createView(tr, tv2, addedItems[i]);
+			createView(tr, tv3, Double.toString(price[i]));
 			t1.addView(tr);
 		}
+		
+		//adding the last row for the total amount using dummy data
+		TableRow tr2 = new TableRow(this);
+		TextView tv4 = new TextView(this);
+		TextView tv5 = new TextView(this);
+		TextView tv6 = new TextView(this);
+		createView(tr2, tv4, "Total:");
+		createView(tr2, tv5, "   ");
+		createView(tr2, tv6, Double.toString(price[0] + price[1] + price[2] + price[3] ));
+		t1.addView(tr2);
 	}
 
 	@Override
@@ -41,16 +55,23 @@ public class CartActivity extends Activity {
 		return true;
 	}
 
-public void createView(TableRow tr, TextView t, String viewdata) {
-	t.setText(viewdata);
-	//adjust the properties of the textView
-	t.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT));
-	t.setTextColor(Color.WHITE);
-	t.setBackgroundColor(Color.BLACK);
-	t.setPadding(20, 0, 0, 0);
-	tr.setPadding(0, 1, 0, 1);
-	tr.setBackgroundColor(Color.WHITE);
-	tr.addView(t); // add TextView to row.
+	public void createView(TableRow tr, TextView t, String viewdata) {
+		//content of each cell
+		t.setText(viewdata);
+		t.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
+				LayoutParams.WRAP_CONTENT));
+		t.setTextColor(Color.WHITE);
+		t.setBackgroundColor(Color.BLACK);
+		//creating borders
+		t.setPadding(20, 0, 0, 0);
+		tr.setPadding(0, 1, 0, 1);
+		tr.setBackgroundColor(Color.WHITE);
+		//add textview to row
+		tr.addView(t);
 	}
 
+    public void home(View v){
+    	Intent intent = new Intent(this, MainActivity.class);
+    	startActivity(intent);
+    }
 }
